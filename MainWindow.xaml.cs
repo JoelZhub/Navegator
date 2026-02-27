827cd385c9216c53e275624449358f9c67bbd122
Joel <joelalbertobenitezvarela28@gmail.com>
2026-02-27 17:12:18 -0400

    fead: terminacion del buscador, apartado de ayuda  y configuracion de elementos de botones de retorno

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Policy;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,7 +11,15 @@ namespace Navegator
     {
         private string searchUrl = "";
 
-        private List<string> urls = new List<string>();   
+        private Stack<string> backUrl = new Stack<string>();
+
+        private Stack<string> fowardUrl = new Stack<string>();
+
+        private bool isNavigate = false;
+
+        private int index = -1;
+
+        private bool help = false;
 
         public MainWindow()
         {
@@ -32,13 +41,20 @@ namespace Navegator
                 bool valid = Regex.IsMatch(url, @"^[a-zA-Z0-9]+\.com$");
                 if (valid)
                 {
-          
+                    backUrl.Push(url);
+                    searchUrl = url;
                     webView.Source = new Uri(url);
+                    index++;
                 }
                 string urlBase = $"https://www.google.com/search?q={Uri.EscapeDataString(url!)}";
-                webView.Source = new Uri(urlBase);
+                searchUrl = urlBase;
+                backUrl.Push(searchUrl);
+                fowardUrl.Clear();
+                webView.Source = new Uri(searchUrl);
+                index++;
                 webView.Visibility = Visibility.Visible;
                 main.Visibility = Visibility.Hidden;
+                isNavigate = true;
             }
         }
 
@@ -67,6 +83,76 @@ namespace Navegator
             }
         }
 
-        
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if(sender is  Button button)
+            {
+              
+                if(button.Tag.ToString() == "btnRefresh" && !isNavigate)
+                {
+                    main.Visibility = Visibility.Visible;
+                    webView.Visibility  =Visibility.Hidden;
+                }
+                else if(button.Tag.ToString() == "btnRefresh" && isNavigate)
+                {
+                    webView.Source = new Uri(searchUrl);
+                }
+                if(backUrl.Count() > 0 && button.Tag.ToString() == "btnBefore")
+                {
+                    fowardUrl.Push(searchUrl);
+                    searchUrl = backUrl.Pop();
+                    index--;
+                    if (index == -1)
+                    {
+                        main.Visibility = Visibility.Visible;
+                        webView.Visibility = Visibility.Hidden;
+                    }
+                }
+                else if(fowardUrl.Count() > 0 && button.Tag.ToString() == "btnAfter")
+                {
+                    backUrl.Push(searchUrl);
+                    searchUrl = fowardUrl.Pop();
+                    index++;
+                }
+
+
+            }
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Button mouseEnter)
+            {
+                if (!isNavigate)
+                    mouseEnter.Cursor = Cursors.Hand;
+            }
+        }
+
+        private void Button_Click_Help(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button btn)
+            {
+                if (!help)
+                {
+                    ayudaPage.Visibility = Visibility.Visible;
+                    main.Visibility = Visibility.Hidden;
+                    webView.Visibility = Visibility.Hidden;
+                    help = true;
+                }
+                else
+                {
+                    ayudaPage.Visibility = Visibility.Hidden;
+                    help = false;
+                    main.Visibility = Visibility.Visible; 
+                }
+            }
+
+        }
     }
 }
